faf5c5f761917d2b123ecf32d0933898c9b320f3
ohwk123 <seojeong@sookmyung.ac.kr>
2022-10-11 17:32:23 +0900

    05

--- main.c
+++ main.c
@@ -2,13 +2,26 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int square(int a)
-{
-	return(a*a);
+int factorial(int n){
+	int i;
+	int sum=1;
+	for(i=1;i<=n;i++)
+	
+	 sum *= i; 
+	 return(sum);
+	 
 }
+int combination(a,b){
+	int result;
+	result = factorial(a)/(factorial(a-b)*factorial(b));
+	return result;
+}
+
+
 int main(int argc, char *argv[])
-{
-	int a = 2;
-	a = square(a);
-	printf("a=%i\n", a);
+{   int x,y,z;
+	printf("put two numbers:");
+	scanf("%i %i", &x, &y);
+	z = combination(x,y);
+	printf("C(%i,%i):%i", x, y, z);
 }
